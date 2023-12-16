e8ee63f4767561f5205b6a914961759221ead7ea
royamousavi <roya2002_rm@yahoo.com>
2023-12-16 22:01:22 +0200

    create section about at index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Slider from '../components/Templates/Index/Slider'
+import Slider from "../components/Templates/Index/Slider";
+import About from "@/components/Templates/Index/About"
 
 export default function Index() {
   return (
     <>
       <Slider />
+      <About />
     </>
   );
 }
