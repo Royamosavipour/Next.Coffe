3109aeec4df76edfb354e1275035b2c976ad6edf
royamousavi <roya2002_rm@yahoo.com>
2023-12-17 16:25:09 +0200

    add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,23 @@ import Slider from "../components/Templates/Index/Slider";
 import About from "@/components/Templates/Index/About";
 import Servises from "@/components/Templates/Index/Servises";
 
-export default function Index() {
+export default function Index({ data }) {
   return (
     <>
       <Slider />
       <About />
-      <Servises />
+      <Servises services={data.services} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:4000/services`);
+  const services = await res.json();
+
+
+  return {
+    props: { data: { services } },
+    revalidate: 60 * 60 * 12,
+  };
 }
